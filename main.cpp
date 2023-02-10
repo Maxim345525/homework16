c8f520aecc24d9de6354c1efb1e581a9d9e87873
Maxim345525 <maximmaksimchyk@gmail.com>
2023-02-10 19:20:54 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
-a
+#include<iostream>
+using namespace std;
+int main()
+{
+	int matr[5][5];
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			matr[i][j] = rand();
+			cout << matr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
