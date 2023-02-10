4ec7f18f5b4d6262d25a69bb40e7cff719a1fe7e
Maxim345525 <maximmaksimchyk@gmail.com>
2023-02-10 19:00:14 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,1 +1,11 @@
-a
+#include <iostream>
+using namespace std;
+int main()
+{
+    int Matr[1][2] = { 50 };
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 4; j++)
+            Matr[i][j] = 0;
+}
