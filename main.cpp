9c9156bc076ae7e9821083f1142774aeef632db7
Maxim345525 <maximmaksimchyk@gmail.com>
2023-02-10 20:14:56 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -2,10 +2,15 @@
 using namespace std;
 int main()
 {
-    int Matr[1][2] = { 50 };
+    char Matr[9][9];
+    Matr[2][5] = 3;
     int i, j;
 
-    for (i = 0; i < 3; i++)
-        for (j = 0; j < 4; j++)
-            Matr[i][j] = 0;
+    for (i = 0; i < 9; i++)
+    {
+        for (j = 0; j < 9; j++)
+        {
+            Matr[i][j] = 7;
+        }
+    }
 }
