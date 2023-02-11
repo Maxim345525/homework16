9842a28460ccc98a3f0349d7c3cb069bae07719f
Maxim345525 <maximmaksimchyk@gmail.com>
2023-02-11 21:32:12 +0200

    add code

--- main.cpp
+++ main.cpp
@@ -1,78 +1,41 @@
-//#include <iostream>
-//using namespace std;
-//int main()
-//{
-//	int array;
-//	int max, max_i, max_j, min, min_i, min_j;
-//	max,min = array[0][0];
-//	max_i,min_i = 0;							
-//	max_j,min_j = 0;
-//	int N, M;
-//	for (int i = 0; i < N; i++) {		
-//		for (int j = 0; j < M; j++) {	
-//			if (array[i][j] > max) {	
-//				max = array[i][j];		
-//				max_i = i;				
-//				max_j = j;
-//			}
-//			if (array[i][j] < min)
-//			{
-//				min = array[i][j];
-//				min_i = i;
-//				min_j = j;
-//			}
-//		}
-//	}
-//	cout << "   Maximum:  " << max << endl;
-//	cout << "   situated  " << max_i + 1 << " radok " << max_j + 1 << " stovb" << endl;
-//	cout << "  Minimum:  " << min << endl;
-//	cout << "   situated " << min_i + 1 << " radok " << min_j + 1 << " stovb" << endl;
-//}
-#include <iostream>
-#include <time.h>
-#include <iomanip>
+
+#include<iostream>
 using namespace std;
-void main() {
-    int tmp;/*створюю тимчасову зміну*/
-    const int rows = 4, cols = 5;/*створюю константу*/
-    int ar[rows][cols];/*створюю цикл*/
-    srand(time(0));/*створюю точку відліку для генеруваня рендомних чисел*/
-    cout << "rows: " << rows << "\ncols: " << cols << "\n";
-    for (int i = 0; i < rows; i++) {/*заповнюю масив рендомними числами*/
-        for (int j = 0; j < cols; j++) {
-            ar[i][j] = rand() % 101 - 50;
-        }
-    }
-    for (int i = 0; i < rows; i++) {/*виводжу на екран початковий масив*/
-        for (int j = 0; j < cols; j++) {
-            cout << setw(5) << ar[i][j];
-        }
-        cout << "\n";
-    }
-    cout << "\n";
-    for (int a = 0; a < rows; a++) {/*сортую масив*/
-        for (int i = 0; i < rows; i++) {
-            for (int j = cols - 1; j > 0; j--) {
-                if (true) {
-                    if (ar [i - 1][j] > ar[i][j + 1]) {
-                        tmp = ar[i - 1][j];
-                        ar[i - 1][j] = ar[i][j + 1];
-                        ar[i][j + 1] = tmp;
-                    }
-                }
-                if (ar[i][j - 1] > ar[i][j]) {
-                    tmp = ar[i][j];
-                    ar[i][j] = ar[i][j - 1];
-                    ar[i][j - 1] = tmp;
-                }
-            }
-        }
-    }
-    for (int i = 0; i < rows; i++) {/*виводжу на екран посортований масив*/
-        for (int j = 0; j < cols; j++) {
-            cout << setw(5) << ar[i][j];
-        }
-        cout << "\n";
-    }
-    system("pause");
+int main()
+{
+	const int n = 4;
+	int arr[n][n];
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			arr[i][j] = rand() % 10;
+			cout << arr[i][j] << " ";
+
+		}
+		cout << endl;
+	}
+	cout << "Main diagonal:";
+	int min, max;
+	min = max = arr[0][0];
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (i == j)
+			{
+				cout << arr[i][j] << " ";
+				if (arr[i][j] < min)
+				{
+					min = arr[i][j];
+				}
+				if (arr[i][j] > max)
+				{
+					max = arr[i][j];
+				}
+			}
+		}
+	}
+	cout << "min:" << min << endl;
+	cout << "max:" << max << endl;
 }
